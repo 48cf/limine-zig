c94d35f1f116e9e8a7943f30712901760c3d7d96
48cf <32851089+48cf@users.noreply.github.com>
2024-01-11 04:48:48 +0100

    Support for recent build system changes

--- build.zig
+++ build.zig
@@ -1,5 +1,17 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("limine", .{ .source_file = .{ .path = "limine.zig" } });
+    const CreateOptions = std.Build.Module.CreateOptions;
+    var options: CreateOptions = .{};
+
+    const root_source_path: std.Build.LazyPath = .{ .path = "limine.zig" };
+    if (@hasField(CreateOptions, "source_file")) {
+        options.source_file = root_source_path;
+    } else if (@hasField(CreateOptions, "root_source_file")) {
+        options.root_source_file = root_source_path;
+    } else {
+        @compileError("unsupported zig version");
+    }
+
+    _ = b.addModule("limine", options);
 }
