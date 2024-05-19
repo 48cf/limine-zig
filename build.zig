37633fbad88d9e4ce5d933d9b158bf242d244469
iretq <32851089+48cf@users.noreply.github.com>
2024-05-20 00:48:18 +0200

    Clean up build.zig
    Since 489c3ebd32d3874f1ad40557d71b72666753ca3f we're already breaking
    compatibility with older Zig versions, which I tried to keep by checking
    which version of `std.Build.Module.CreateOptions` the build system uses.
    
    In this case I'm removing the backward compatibility trick as it's now
    effectively useless. If you're on such old Zig version, please update!

--- build.zig
+++ build.zig
@@ -1,17 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const CreateOptions = std.Build.Module.CreateOptions;
-    var options: CreateOptions = .{};
-
-    const root_source_path: std.Build.LazyPath = b.path("limine.zig");
-    if (@hasField(CreateOptions, "source_file")) {
-        options.source_file = root_source_path;
-    } else if (@hasField(CreateOptions, "root_source_file")) {
-        options.root_source_file = root_source_path;
-    } else {
-        @compileError("unsupported zig version");
-    }
-
-    _ = b.addModule("limine", options);
+    _ = b.addModule("limine", .{
+        .root_source_file = b.path("limine.zig"),
+    });
 }
